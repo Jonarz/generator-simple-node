84e4a693aa9e22cebd7da42a82d884dd28fe13a2
Camilo Andrés Elgueta Basso <30680798+caeb92@users.noreply.github.com>
2020-02-23 14:50:28 -0300

    Fix db index.js
    Changes now are just applied only to environment variables and constants

--- generators/app/templates/src/db/index.js
+++ generators/app/templates/src/db/index.js
@@ -3,25 +3,44 @@ import path from "path";
 import Sequelize from "sequelize";
 import config from "../config/config";
 import constants from "../config/constants";
+
 const basename = path.join(__dirname, "../models");
 const db = {};
 
 let sequelize;
-sequelize = new Sequelize(
-  config.database.name,
-  config.database.user,
-  config.database.password,
-  {
-    host: config.database.host,
-    port: config.database.port,
-    dialect: constants.DATABASES.POSTGRES,
-    dialectOption: {
-      ssl: true,
-      native: true
-    },
-    logging: true
-  }
-);
+// Production configuration
+if (config.app.environment === constants.PRODUCTION) {
+  sequelize = new Sequelize(
+    config.database.name,
+    config.database.user,
+    config.database.password,
+    {
+      host: config.database.host,
+      port: config.database.port,
+      dialect: constants.DATABASES.POSTGRES,
+      dialectOption: {
+        ssl: true,
+        native: true
+      },
+      logging: true
+    }
+  );
+} else {
+  // Developement configuration
+  sequelize = new Sequelize(
+    config.database.name,
+    config.database.user,
+    config.database.password,
+    {
+      dialect: constants.DATABASES.SQLITE,
+      storage: ":memory",
+      dialectOption: {
+        ssl: true,
+        native: true
+      }
+    }
+  );
+}
 
 fs.readdirSync(basename).forEach(file => {
   const model = sequelize.import(path.join(basename, file));
