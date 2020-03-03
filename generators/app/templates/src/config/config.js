e392e99bf45bd882dfe0e63b1cb6989e69b5d0e2
Jonarz <ramirez.jonathan18@gmail.com>
2020-03-02 23:09:05 -0300

    FIX: remove database on general config file

--- generators/app/templates/src/config/config.js
+++ generators/app/templates/src/config/config.js
@@ -14,13 +14,6 @@ export default {
     refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
     expirationTime: process.env.TOKEN_EXPIRATION_TIME
   },
-  database: {
-    name: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST
-  },
   logger: {
     levelDev: process.env.LOGGER_LEVEL_DEV,
     levelProd: process.env.LOGGER_LEVEL_PROD
